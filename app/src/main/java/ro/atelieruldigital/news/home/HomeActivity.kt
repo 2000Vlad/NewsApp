f3770d6229564017c14cf7bf3fafb109e9a20eb3
2000Vlad <iv2000ro@gmail.com>
2019-12-08 02:13:33 +0200

    News Item

--- app/src/main/java/ro/atelieruldigital/news/home/HomeActivity.kt
+++ app/src/main/java/ro/atelieruldigital/news/home/HomeActivity.kt
@@ -16,23 +16,36 @@ class HomeActivity : BaseActivity() {
     protected override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        val recyclerView = findViewById<RecyclerView>(R.id.news_rv)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = object : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-                return object : RecyclerView.ViewHolder(
-                        LayoutInflater.from(this@HomeActivity).inflate(R.layout.basic_search_layout, parent, false)
-                ){}
-            }
 
-            override fun getItemCount(): Int {
-                return 100
-            }
+    }
+}
 
-            override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+fun ellipsize(string: String, size: Int): String {
 
+    val words = string.split(" ")
+    if (words.size == 1)
+        if (words[0].length > 40) {
+            return String(words[0].toCharArray().apply {
+                this[words[0].length - 1] = '.'
+                this[words[0].length - 2] = '.'
+                this[words[0].length - 3] = '.'
             }
+            )
+        } else return words[0]
+    else {
+        var sum = 0
+        var result = ""
+        for (i in words.indices) {
+            if (sum + words[i].length >= size) {
+                return result.padEnd(3,'.')
+            }
+            result += words[i]
+            sum += words[i].length
         }
     }
 
+    return string
+
 }
+
+const val MAX_TITLE_SIZE = 40
